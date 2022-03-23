23470e9311eacb98a3cc071bd45031bcc609714f
stefanoprea02 <stefanoprea02@yahoo.com>
2022-03-23 23:17:36 +0200

    .

---
+++ scripts.js
@@ -0,0 +1,65 @@
+function readMore1(){
+    document.querySelector('#content1').style.height= 'auto';
+    document.querySelector('#readMore1').style.display = 'none';
+    document.querySelector('#readLess1').style.display = 'block';
+}
+function readLess1(){
+    document.querySelector('#content1').style.height= '2em';
+    document.querySelector('#readMore1').style.display = 'block';
+    document.querySelector('#readLess1').style.display = 'none';
+}
+
+function readMore2(){
+    document.querySelector('#content2').style.height= 'auto';
+    document.querySelector('#readMore2').style.display = 'none';
+    document.querySelector('#readLess2').style.display = 'block';
+}
+function readLess2(){
+    document.querySelector('#content2').style.height= '2em';
+    document.querySelector('#readMore2').style.display = 'block';
+    document.querySelector('#readLess2').style.display = 'none';
+}
+
+function readMore3(){
+    document.querySelector('#content3').style.height= 'auto';
+    document.querySelector('#readMore3').style.display = 'none';
+    document.querySelector('#readLess3').style.display = 'block';
+}
+function readLess3(){
+    document.querySelector('#content3').style.height= '2em';
+    document.querySelector('#readMore3').style.display = 'block';
+    document.querySelector('#readLess3').style.display = 'none';
+}
+
+function readMore4(){
+    document.querySelector('#content4').style.height= 'auto';
+    document.querySelector('#readMore4').style.display = 'none';
+    document.querySelector('#readLess4').style.display = 'block';
+}
+function readLess4(){
+    document.querySelector('#content4').style.height= '2em';
+    document.querySelector('#readMore4').style.display = 'block';
+    document.querySelector('#readLess4').style.display = 'none';
+}
+
+function readMore5(){
+    document.querySelector('#content5').style.height= 'auto';
+    document.querySelector('#readMore5').style.display = 'none';
+    document.querySelector('#readLess5').style.display = 'block';
+}
+function readLess5(){
+    document.querySelector('#content5').style.height= '2em';
+    document.querySelector('#readMore5').style.display = 'block';
+    document.querySelector('#readLess5').style.display = 'none';
+}
+
+function readMore6(){
+    document.querySelector('#content6').style.height= 'auto';
+    document.querySelector('#readMore6').style.display = 'none';
+    document.querySelector('#readLess6').style.display = 'block';
+}
+function readLess6(){
+    document.querySelector('#content6').style.height= '2em';
+    document.querySelector('#readMore6').style.display = 'block';
+    document.querySelector('#readLess6').style.display = 'none';
+}
